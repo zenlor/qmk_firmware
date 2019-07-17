ba9b338b125be5656488674aa4ff045e37f7575e
Lorenzo Giuliani <lorenzo.giuliani@myjar.com>
2019-07-17 16:31:17 +0300

    stuck in _LW layer

--- keyboards/atreus/keymaps/zenlor/keymap.c
+++ keyboards/atreus/keymaps/zenlor/keymap.c
@@ -10,14 +10,15 @@
 #define _QW 0
 #define _RS 1
 #define _LW 2
+#define _GA 3
+#define _GB 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
-  ),
+    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT  ),
 
   /*
    *  !       @     up     {    }        ||     pgup    7     8     9    *
@@ -39,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LW] = LAYOUT( /* [> LOWER <] */
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
     KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,                     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
+    TO(_QW), KC_VOLU, KC_NO,   KC_NO,   RESET,                     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
     TO(_GA), KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS ),
 
   /*
@@ -59,8 +60,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TO(_QW), KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_TAB, KC_SPC, MO(_GB), KC_S, KC_V, KC_F ),
 
   [_GB] = LAYOUT( /* [> GAMING B <] */
-    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                  KC_9,   KC_F4,   KC_5,  KC_6,  KC_7  ,
-    KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                  KC_0,   KC_F1,   KC_F2, KC_F3, KC_8  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   KC_B,                     KC_GRV, KC_Q,    KC_W,  KC_E,  KC_MINS  ,
-    TO(_QW), KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_TAB, KC_SPC, KC_TRNS, KC_S,  KC_V,  KC_F ),
+    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                  C(KC_R), KC_F4,   KC_F5, KC_F6, A(KC_Q) ,
+    KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                  C(KC_T), KC_F1,   KC_F2, KC_F3, A(KC_W) ,
+    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   KC_B,                     C(KC_Y), KC_Q,    KC_W,  KC_E,  A(KC_E) ,
+    TO(_QW), KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_TAB, KC_SPC,  KC_TRNS, KC_S,  KC_V,  A(KC_R) )
 };
