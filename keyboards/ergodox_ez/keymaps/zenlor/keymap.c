1e60f950af39c1f655b720997cf39a31accd6773
Lorenzo Giuliani <lorenzo@frenzart.com>
2021-01-04 09:50:13 +0000

    more tweaks

--- keyboards/ergodox_ez/keymaps/zenlor/keymap.c
+++ keyboards/ergodox_ez/keymaps/zenlor/keymap.c
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_ESCAPE,   KC_INSERT,   KC_LGUI, KC_LSHIFT,
                                                                        KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                        KC_TRANSPARENT,
-                                                                   KC_BSPACE, KC_LCTL, KC_TRANSPARENT,
+                                                                   KC_DELETE, KC_LCTL, KC_TRANSPARENT,
 
     KC_TRANSPARENT, KC_F6,     KC_F7,          KC_F8,  KC_F9, KC_F10,    KC_F11,
     KC_TRANSPARENT, KC_PGUP,   KC_7,           KC_8,   KC_9,  KC_ASTR,   KC_F12,
@@ -229,7 +229,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_T,         KC_7,    KC_8,  KC_9,  KC_0,  KC_MINUS, KC_EQUAL,
                   KC_1,    KC_2,  KC_3,  KC_4,  KC_5,     KC_6,
     LSFT(KC_TAB), KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_GRAVE, KC_R,
-                           KC_Z,  KC_X,  KC_S,  KC_V,     KC_LCTL,
+                           KC_Z,  KC_S,  KC_X,  KC_V,     KC_LCTL,
     KC_M, KC_ESCAPE,
     KC_C,
     KC_LALT, KC_LSHIFT, KC_SPACE),
